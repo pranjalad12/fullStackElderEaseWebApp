09ef39eabcf6fd8c6368150eae87470781144fa7
pranjalad12 <lci2021021@iiitl.ac.in>
2024-03-01 22:46:01 +0530

    position of button to left

--- frontend/components/Contact/index.jsx
+++ frontend/components/Contact/index.jsx
@@ -470,26 +470,16 @@ const Contact = () => {
                   />
                 </div>
               </div>
+              <div className="relative">
               <button
                 onClick={() => bothFxn()}
                 aria-label="send message"
-                className="inline-flex items-center gap-2.5 rounded-full bg-black px-6 py-3 font-medium text-white duration-300 ease-in-out hover:bg-blackho dark:bg-btndark"
+                className="inline-flex items-center gap-2.5 rounded-full bg-black px-6 py-3 font-medium text-white duration-300 ease-in-out hover:bg-blackho dark:bg-btndark absolute top-0 right-0 mb-20"
               >
                 Save Preferences
-                <svg
-                  className="fill-white"
-                  width="14"
-                  height="14"
-                  viewBox="0 0 14 14"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M10.4767 6.16664L6.00668 1.69664L7.18501 0.518311L13.6667 6.99998L7.18501 13.4816L6.00668 12.3033L10.4767 7.83331H0.333344V6.16664H10.4767Z"
-                    fill=""
-                  />
-                </svg>
+               
               </button>
+              </div>
             </motion.div>
           </div>
         </div>
