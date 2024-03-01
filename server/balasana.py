540d46fc11332bdd13a3e6a082b4291908a7cab0
Bhavya <lci2021043@iiitl.ac.in>
2024-03-01 16:41:47 +0530

    balasana labels and voice

--- server/balasana.py
+++ server/balasana.py
@@ -1,10 +1,16 @@
 import math
 import cv2
 import mediapipe as mp
+import pyttsx3
 
 mp_pose = mp.solutions.pose
 pose = mp_pose.Pose(static_image_mode=True, min_detection_confidence=0.3, model_complexity=2)
 mp_drawing = mp.solutions.drawing_utils
+
+engine = pyttsx3.init()
+def speak_label(label):
+    engine.say(label)
+    engine.runAndWait()
 
 def calculateAngle(landmark1, landmark2, landmark3):
     x1, y1, _ = landmark1
@@ -15,24 +21,29 @@ def calculateAngle(landmark1, landmark2, landmark3):
 
     return angle
 
+prev_label = "prev"
 def giveLabelForBalasanaPose(left_elbow_angle, right_elbow_angle, left_shoulder_angle, right_shoulder_angle, left_knee_angle, right_knee_angle, left_hip_angle, right_hip_angle):
+    global prev_label
     label = 'Unknown'
     
     if (left_elbow_angle > 160 and left_elbow_angle < 220) or (right_elbow_angle >140  and right_elbow_angle < 200):
         if (left_shoulder_angle > 120 and left_shoulder_angle < 190) or (right_shoulder_angle > 120 and right_shoulder_angle < 190):  
             if (left_hip_angle > 305 and left_hip_angle < 355) or (right_hip_angle > 0 and right_hip_angle < 50):
                 if (left_knee_angle > 5 and left_knee_angle < 55) or (right_knee_angle > 300 and right_knee_angle < 350):
-                    label = 'Balasana'
+                    label = 'Thats it, Perfect Balasana'
                 else:
-                    label = 'KNEE'
+                    label = 'Straighten up your knees a little'
             else:
-                label = 'HIPS'
+                label = 'Bend from hips a little more to achieve Balasana'
         else:
-            label = 'SHOULDERS'
+            label = 'Align your shoulders well'
     else:
-        label = 'ELBOWS'
-        
-    return label
+        label = 'Strech you elbows a bit more.'
+    
+    if(label!=prev_label): 
+        speak_label(label)
+        prev_label=label
+    return label  
 
 def classifyBalasanaPose(landmarks, output_image, display=False):
     label = 'Unknown Pose'
@@ -48,7 +59,7 @@ def classifyBalasanaPose(landmarks, output_image, display=False):
     right_hip_angle = calculateAngle(landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value], landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value],landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value])
     
     label = giveLabelForBalasanaPose(left_elbow_angle, right_elbow_angle, left_shoulder_angle, right_shoulder_angle, left_knee_angle, right_knee_angle, left_hip_angle, right_hip_angle)
-    if label == 'Balasana': color = (0,255,0)
+    if label == 'Thats it, Perfect Balasana': color = (0,255,0)
     else: color=(0,0,255)
 
     cv2.putText(output_image, label, (10, 30),cv2.FONT_HERSHEY_PLAIN, 2, color, 5)
